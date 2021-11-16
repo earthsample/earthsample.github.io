ac2c8bb68ba1283cdb50c07c22eff4c2fbfac8e3
danielmacneilportfolio <58915636+danielmacneilportfolio@users.noreply.github.com>
2021-11-16 17:52:20 -0500

    starting fresh

--- JAVASCRIPT/script.js
+++
@@ -1,9 +0,0 @@
-/*eslint-env es6*/
-/*global document*/
-
-const hamburger = document.getElementById('hamburger');
-const HomeList = document.getElementById('HomeList');
-
-hamburger.addEventListener('click', () => {
-    HomeList.classList.toggle('show');
-});
